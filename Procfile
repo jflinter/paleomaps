66cb9671f450d73f65c20201fef3200588f02dfd
Jack Flintermann <jflinter11@gmail.com>
2012-03-13 00:35:00 -0700

    procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python paleo/manage.py collectstatic --noinput; python paleo/manage.py run_gunicorn -b 0.0.0.0:$PORT -w 3
--- paleo/settings.py
+++ paleo/settings.py
@@ -8,6 +8,7 @@ TEMPLATE_DEBUG = DEBUG
 
 ROOT_PATH = abspath(dirname(__file__))
 PROJECT_NAME = basename(ROOT_PATH)
+PROJECT_PATH = dirname(ROOT_PATH)
 
 ADMINS = (
     ('Jack Flintermann', 'jflinter11@gmail.com'),
@@ -78,7 +79,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = join(PROJECT_PATH, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -137,6 +138,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'paleo_webapp',
+    'gunicorn',
 )
 
 # A sample logging configuration. The only tangible logging
