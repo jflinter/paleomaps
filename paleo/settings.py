ffc0a06a8614f4df4450fe230d174c43805af1a4
Jack Flintermann <jflinter11@gmail.com>
2012-03-12 20:21:59 -0700

    settings and reqs

--- paleo/settings.py
+++ paleo/settings.py
@@ -137,7 +137,6 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'paleo_webapp',
-    'djangotoolbox',
 )
 
 # A sample logging configuration. The only tangible logging
